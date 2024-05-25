7dd8f2b5d2b027d14d2eca0bdaacf8c5a4b42e91
Madhav Dhingra <madhavdhingra11@gmail.com>
2024-05-26 00:33:29 +0530

    Completed ui design

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import SearchInput from './SearchInput'
+import Conversations from './Conversations'
+import LogoutButton from './LogoutButton'
 
 function Sidebar() {
-  return <div>
+  return <div className='border-r border-slate-500 p-4 flex flex-col'>
       
       <SearchInput />
       <div className='divider px-3'></div>
-      {/* <Conversation />
-      <LougoutButton /> */}
+      <Conversations />
+      <LogoutButton />
     </div>
 }
 
